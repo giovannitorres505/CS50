cdc2225ca9938c5b3f43ac48e326f19b24e9145d
Giovanni Torres <109697844+giovannitorres505@users.noreply.github.com>
2026-04-06 21:49:32 -0300

    Update models.py

--- project2/auctions/models.py
+++ project2/auctions/models.py
@@ -1,1 +1,42 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
+class User(AbstractUser):
+    pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="listings", blank=True, null=True)
+    active = models.BooleanField(default=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    watchlist = models.ManyToManyField(User, blank=True, related_name="watched_items")
+
+    def __str__(self):
+        return self.title
+
+class Bid(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+
+    def __str__(self):
+        return f"{self.user} bid {self.amount} on {self.listing}"
+
+class Comment(models.Model):
+    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} on {self.listing}"
